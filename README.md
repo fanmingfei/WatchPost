b9bad9f28e64a677f856847332516e124829ff56
edire <az8641683@163.com>
2015-07-25 00:25:14 +0800

    init

---
+++ README.md
@@ -0,0 +1,41 @@
+# 本地开发过程中课直接与服务器进行文件同步
+
+版本： v1.0.0
+
+## 使用方法：
+
+    先安装node、npm等工具~
+
+    先填写好config.json 文件，然后在目录下执行 node main.js
+
+    目前没有测试过Windows
+
+
+### config.js 参数简介
+
+    {
+        "receiver": "http://jiaowu/",
+        "base": "/Users/fanmingfei/ldsnwork/server/",
+        "to": "/data/www/jiaowu/server",
+        "unwatchSuffix": [".swp"],
+        "unwatchPath": [".git","Runtime"]
+    }
+
+
+    * receiver 接收文件的url
+
+    本插件是通过post请求发送文件，所以服务端需要一个文件进行接收，文件是从FEX的FIS库中直接拿过来的，
+
+    链接：<https://github.com/fex-team/fis3-command-release/blob/master/tools/receiver.php>
+
+    直接放在服务器可以访问到的地方，把访问URL放在receiver
+
+    * base 本机项目目录
+
+    * to 测试机项目目录
+
+    * unwatchSuffix 不进行监控的文件后缀名， 比如说一些没有用的 可能会出现的 .psd、.zip 之类的都不需要监控，注意这里是带"."的。
+
+    * unwatchPath 不进行监控的目录，有些目录不需要进行监控，因为每次开启监控都要重新吧所有文件上传一次，像一些无需第一次更新的目录卸载上面，就不会在执行代码的时候把它上传上了，但是开始监控以后会实时监控这些目录。
+
+
---
+++ watchPost.js
@@ -0,0 +1,114 @@
+/**
+ * watchPost.js
+ * author: fanmingfei@ldsn
+ */
+var fs = require('fs');
+var path = require('path');
+var watch = require('./lib/watch.js');
+var upload = require('./lib/upload.js');
+
+var config = JSON.parse(fs.readFileSync('./config.json'));
+var base = config.base;
+var to = config.to;
+var unwatchSuffix = config.unwatchSuffix;
+var unwatchPath = config.unwatchPath;
+var receiver = config.receiver;
+var option = function (f, curr, prev) {
+    if (typeof f == "object" && prev === null && curr === null) {      // Finished walking the tree
+        var files = [];
+        for (x in f) {
+            var stat = fs.statSync(x);
+            if (!stat.isDirectory()) {
+                files.push(x);
+            }
+        }
+        uploadEach(files);
+    } else if (prev === null) {
+        analyse(f, function (data, f) {
+            if (data != 0) {
+                console.log('文件传输失败');
+            } else {
+                console.log(f, '传输成功！')
+            }
+        });
+    } else if (curr.nlink === 0) {
+    } else {
+        analyse(f, function (data) {
+            if (data != 0) {
+                console.log('文件传输失败');
+            } else {
+                console.log(f, '传输成功！')
+            }
+        });
+    }
+}
+var errorFile = [];
+var eachI = -1;
+var uploadEach = function (files) {
+    eachI ++;
+    analyse(x, function (f) {
+        if (eachI == files.length) {
+            startWatch();
+            return;
+        }
+        var suffixFlag = false;
+        var pathFlag = false;
+        unwatchSuffix.forEach(function (x) {
+            if (path.extname(files[eachI]) === x) {
+                suffixFlag = true;
+            }
+        });
+        unwatchPath.forEach(function (x) {
+            if (files[eachI].split(path.sep).indexOf(x) > -1) {
+                pathFlag = true;
+            }
+        });
+        if (suffixFlag || pathFlag) {
+            if (eachI < files.length) {
+                uploadEach(files);
+                return;
+            }
+        }
+        if (f != 0) {
+            console.log(files[eachI], 'x>', path.join(to, path.relative(base, files[eachI])));
+            errorFile.push(files[eachI]);
+            if (eachI < files.length) {
+                uploadEach(files)
+                return;
+            }
+        } else {
+            console.log(files[eachI], '->', path.join(to, path.relative(base, files[eachI])));
+
+            if (eachI < files.length) {
+                uploadEach(files)
+                return;
+            } else {
+                startWatch();
+            }
+        }
+    });
+};
+
+var startWatch = function () {
+    if (errorFile.length > 0) {
+        console.log('-----传输失败的文件-----')
+        errorFile.forEach(function (x) {
+            console.log(x);
+        });
+        console.log('-----传输失败的文件-----');
+    }
+    console.log('--------开始监控------')
+};
+
+var analyse = function (f, callback) {
+    var relative =  path.relative(base, f);
+    var toPath = path.join(to, relative);
+    upload(f, toPath, receiver, function (err, data) {
+        if (err) {
+            console.log(err);
+        }
+        callback(data, f);
+    });
+};
+
+watch.watchTree(base, option, unwatchSuffix, unwatchPath);
